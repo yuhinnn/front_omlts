3d5e363006ee16366cb7ee89bc06272164f36915
yuhi_deve <yuhinnn@gmail.com>
2016-11-28 15:07:53 +0900

    add git　ignore

---
+++ index.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>オモロタス</title>
+  <style type="text/css">
+body{
+  margin: 0;
+}
+.relative{
+ position:relative;
+ margin:0;
+ padding:0;
+ width:100%;
+ text-align:center;
+}
+.absolute {
+    position: absolute;
+    bottom: 2130px;
+    width:100%;
+    text-align:center;
+    font-size:290%;
+    margin:0;
+    padding:0;
+}
+.smooth1{
+    position: absolute;
+    bottom: 4890px;
+
+}
+.smooth2{
+    position: absolute;
+    bottom: 3730px;
+
+}
+.smooth4{
+    position: absolute;
+    bottom: 1985px;
+
+}
+.smooth5{
+    position: absolute;
+    bottom: 6800px;
+
+}
+.page_top{
+   /* padding-top:4550px;*/
+    display:block;
+    margin-right:auto;
+    margin-left:auto;
+    padding-top:30px;
+
+    
+}
+.about{
+    position:absolute;
+    right: 200px;
+    bottom: 4850px;
+}
+.produce{
+    position:absolute;
+    right: 200px;
+    bottom: 2850px;
+}
+.gmail{
+ color:red;
+}
+.gmail:hover{
+ color:blue;
+}
+.content{
+height:2800px;
+}
+.aidl{
+ width:100%;
+ padding-top:20px;
+ 
+}
+
+/*
+.main{
+  background-image:url(img/main.jpg);
+  background-position:center;
+  background-repeat:no-repeat;
+}*/ 
+
+/*
+.main{
+  margin:0 auto;
+  display: block;
+  width:95%;
+}*/ 
+
+
+
+
+.produce{
+  padding-top:9000px;
+}
+
+}
+.logo{
+ padding:20px 0px 0px 50px;
+
+}
+a {
+  text-decoration: none;
+  color: #1f89e2;
+}
+
+a:hover {
+  text-decoration: underline;
+  color: #1b7ccd;
+}
+
+.top-wrapper{
+  background-image: url(img/aidl.jpg);
+  background-size:cover;
+  margin:20px;
+ text-align:center;
+}
+
+.sinki{
+  background-color:red;
+}
+
+.facebook{
+  background-color: #3b5998;
+}
+
+.twitter{
+  background-color: #55acee;
+}
+
+.btn{
+ color: white;
+ opacity:0.8;
+ padding:10px 50px;
+
+}
+.btn:hover{
+  opacity:1;
+}
+
+.header-left{
+  float:left ;
+  color:white;
+  padding-top:10px;
+}
+
+
+.header-right{
+  float:right;
+  margin-right: 30px;
+  padding-top: 5px;
+}
+
+.header-right li{
+  list-style-type: none;
+  float: left;
+  margin: 0 20px;
+}
+
+.header-right a{
+  color:white;
+}
+.header-left a{
+  color:white;
+}
+footer{
+  border-top:1px solid #777777;
+  padding-top:10px;
+
+}
+
+.fotter-right{
+  float:right;
+  margin:20px 200px 0 0;
+}
+
+.fotter-right a{
+  color:black;
+}
+
+.fotter-left a {
+  color:black;
+  margin-left: 10px;
+}
+.msg{
+  background-color:white;
+  padding:0 50px 20px 50px;
+  border:solid 5px;
+  box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.2);
+}
+.wrapper-about{
+  margin:0 auto;
+  background-image: url(sea.jpg);
+  background-size: cover;
+  padding:180px;
+}
+.wrapper-index {
+    margin: 0 auto;
+    background-image: url(tree2.jpg);
+    background-size: cover;
+    padding: 180px;
+}
+.midashi-about{
+  background-color: rgb(236, 236, 236);
+  padding:1px 15px;
+  border-radius: 0.2cm;
+  margin-top:30px;
+  text-align: center;
+
+}
+.wrapper-index{
+  margin:0 auto;
+  background-image: url(tree2.jpg);
+  background-size: cover;
+  padding:180px;
+}
+.midashi-index{
+  background-color: rgb(236, 236, 236);
+  padding:1px 15px;
+  border-radius: 0.2cm;
+  margin-top:30px;
+  text-align: center;
+
+}
+body{
+  margin: 0;
+}
+.wrapper{
+  margin:0 auto;
+  background-image: url(hand.jpg);
+  background-size: cover;
+  padding:180px;
+}
+.msg{
+  background-color:white;
+  padding:0 50px 20px 50px;
+  border:solid 5px;
+  box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.2);
+}
+.midashi{
+  background-color: rgb(236, 236, 236);
+  padding:1px 15px;
+  border-radius: 0.2cm;
+  margin-top:30px;
+  text-align: center;
+}
+
+
+.tytle{
+  display: block;
+}
+
+.content{
+  display: block;
+}
+
+
+.tytle-area{
+  width:50%;
+}
+
+.content-area{
+  height: 180px;
+ width: 70%;
+}
+
+.notes{
+  list-style-type: none;
+}
+.notes li {
+  border-top:1px solid #e8e8e8;
+  border-width:20%;
+  padding:25px 0 25px 100px;
+}
+.alert ul{
+  margin-left:-24px;
+}
+h1 {
+  text-align: center;
+}
+body{
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  color: #1f89e2;
+}
+
+a:hover {
+  text-decoration: underline;
+  color: #1b7ccd;
+}
+.top-container{
+  margin: 0 auto;
+  text-align: center;
+  color: white;
+  padding:20px;
+}
+
+
+
+
+.facebook{
+  background-color: #3b5998;
+}
+
+.twitter{
+  background-color: #55acee;
+}
+
+.btn{
+ color: white;
+ opacity:0.8;
+ padding:10px 50px;
+
+}
+.btn:hover{
+  opacity:1;
+}
+
+header{
+  background-color:#d59e51;
+  width:100%;
+  height: 80px;
+  padding-bottom:0;
+}
+
+
+.header-right{
+  float:right;
+  margin-right: 30px;
+  padding-top: 5px;
+}
+.header-right li{
+  list-style-type: none;
+  float: left;
+  margin: 0 20px;
+}
+
+.header-right a{
+  color:white;
+}
+.header-left a{
+  color:white;
+}
+footer{
+  background-color:#d59e51;
+  width:100%;
+  height: 80px;
+  padding-bottom:0;
+  margin-top:50px;
+}
+
+.fotter-left{
+  float: left;
+}
+
+.fotter-right a{
+  color:black;
+}
+
+.fotter-left a {
+  color:black;
+  margin-left: 10px;
+}
+.msg{
+  background-color:white;
+  padding:0 50px 20px 50px;
+  border:solid 5px;
+  box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.2);
+}
+.wrapper-about{
+  margin:0 auto;
+  background-image: url(sea.jpg);
+  background-size: cover;
+  padding:180px;
+}
+.wrapper-index {
+    margin: 0 auto;
+    background-image: url(tree2.jpg);
+    background-size: cover;
+    padding: 180px;
+}
+.midashi-about{
+  background-color: rgb(236, 236, 236);
+  padding:1px 15px;
+  border-radius: 0.2cm;
+  margin-top:30px;
+  text-align: center;
+
+}
+.wrapper-index{
+  margin:0 auto;
+  background-image: url(tree2.jpg);
+  background-size: cover;
+  padding:180px;
+}
+.midashi-index{
+  background-color: rgb(236, 236, 236);
+  padding:1px 15px;
+  border-radius: 0.2cm;
+  margin-top:30px;
+  text-align: center;
+
+}
+  </style>
+</head>
+<body>
+  <header>
+    <div class="head-container">
+    <div class="header-left">
+    <img class="logo" src="img/headlogo.jpg">
+    </div>
+    <div class="header-right">
+    <ul>
+     <a href="#smoothplay1"><li><font size="5">About</font></li></a>  
+     <a href="#smoothplay2"><li><font size="5">Produce</font></li></a>  
+     <a href="mailto:omlts.abekobe@gmail.com?subject=件名&amp;bodyi=本文"><li><font size="5">Contact</font></li></a>  
+     <a href="#smoothplay4"><li><font size="5">Member</font></li></a>  
+   </ul>
+    </div>
+    </div>
+  </header>
+
+
+<img class="aidl" src="img/aidl.jpg">
+  <div class="top-container">
+    <a href="mailto:omlts.abekobe@gmail.com?subject=件名&amp;bodyi=本文" class="btn sinki">Gmail</a>
+    <a href="https://www.facebook.com/omorotasu/" class="btn facebook">facebook</a>
+    <a href="http://twitter.com/omorotasu3" class="btn twitter">twitter</a>
+  </div>
+</div>
+   <div class="relative">
+    <img class="main" src="img/omlts_img_pc.jpg">
+    <p class="absolute gmail">
+    <a href="mailto:omlts.abekobe@gmail.com?subject=件名&amp;bodyi=本文">omlts.abekobe@gmail.com</a>
+    </p>
+    <p id="smoothplay1" class="smooth1">.</p>
+    <p id="smoothplay2" class="smooth2">.</p>
+    <p id="smoothplay4" class="smooth4">.</p>
+    <p id="smoothplay5" class="smooth5">f</p>
+<!--
+    <p id="smoothplay1" class="about absolute">a</p>
+    <p id="smoothplay2" class="produce absolute">b</p>
+　</div>
+--!>
+
+    <a href="#smoothplay5"><img class="page_top" src="img/page.png"></a>
+<footer>
+
+<div class="fotter-left">
+   <a href="#smoothplay5"><img class="logo" src="img/headlogo.jpg"></a>
+</div>
+
+
+<div id="" class="fotter-right">
+Copyright © 2015-2016 sometimes study All Rights Reserved.
+</div>
+
+</footer>
+
+</body>
+</html>
